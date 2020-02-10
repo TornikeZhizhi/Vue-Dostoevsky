629c3b2ef6ba02d659ff05b564ee62b5d1af9750
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-02-10 17:49:37 +0400

    phrazhes parralax

--- dostoevksy-vue/src/components/HomeComponents/HomePhases.vue
+++ dostoevksy-vue/src/components/HomeComponents/HomePhases.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container-fluid text-center fraz_fluid" v-if="PhrazhesData">
-    <div class="wow fadeInUp2" data-wow-offset="300">
-      <h5 class="book_image2">{{ PhrazhesData }}</h5>
+    <kinesis-container>
+      <kinesis-element :strength="7" type="depth_inv" :distance="10">
+        <div class="wow fadeInUp2" data-wow-offset="300">
+          <h5 class="book_image2">{{ PhrazhesData }}</h5>
 
-      <!-- <span>{{ PhrazhesData.text_ka }}</span> -->
-    </div>
+          <!-- <span>{{ PhrazhesData.text_ka }}</span> -->
+        </div>
+      </kinesis-element>
+    </kinesis-container>
   </div>
 </template>
 
